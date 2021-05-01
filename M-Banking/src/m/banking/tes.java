21092b50e0808f1837323ee5b62b8341444cc695
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-05-01 19:30:16 +0700

    tes4

---
+++ M-Banking/src/m/banking/tes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m.banking;
+
+/**
+ *
+ * @author LINGGO
+ */
+public class tes {
+    private int a;
+
+    public tes() {
+    }
+    
+}
