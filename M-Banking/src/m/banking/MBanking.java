f3dae8a38ae0f7c987fc9d135b3eac7bfa1ee951
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-05-01 21:15:17 +0700

    Update MBanking.java

--- M-Banking/src/m/banking/MBanking.java
+++ M-Banking/src/m/banking/MBanking.java
@@ -3,6 +3,9 @@ package m.banking;
 
 public class MBanking {
 
+    public static int WIDTH = 650;
+    public static int HEIGHT = 900;
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("bruh?");
