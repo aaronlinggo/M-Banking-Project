859fccbb92fcb45f9323cd99b2be27e48e871d48
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-05-01 19:25:14 +0700

    tes3

--- M-Banking/src/m/banking/MBanking.java
+++ M-Banking/src/m/banking/MBanking.java
@@ -6,7 +6,7 @@ public class MBanking {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("bruh?");
-        System.out.println("aaa");
+        System.out.println("abc");
     }
     
 }
