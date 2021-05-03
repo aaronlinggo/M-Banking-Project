77c5449856c5280a0ba613ef39e061cd8d56f61b
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-05-03 22:36:01 +0700

    tes

--- M-Banking/src/UI/AdminHomepage.java
+++ M-Banking/src/UI/AdminHomepage.java
@@ -17,6 +17,7 @@ public class AdminHomepage extends javax.swing.JFrame {
     /**
      * Creates new form AdminHomepage
      */
+    //tes
     public AdminHomepage() {
         initComponents();
         this.setBackground(new Color(0.0f, 0.0f, 0.0f, 0.0f));
